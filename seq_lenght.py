a1b515c905de1a90c85f79a7ec455643cec546d7
Beatrice Cavagnoli <beatrca@login-5.saga>
2021-11-16 14:49:38 +0100

    Reports length and header for all sequences

--- seq_lenght.py
+++ seq_lenght.py
@@ -1,10 +1,17 @@
 import sys
 
-with open(sys.argv[l], "r") as fh:
+with open(sys.argv[1], "r") as fh:
      lines = fh.readlines()
 
 for line in lines:
     line = line.strip()
-    print(len(line))
+    if line.startswith(">"):
+         header = line
+    else:
+        length = len(line)
+        print(length, header)
+
+
+    
 
 
